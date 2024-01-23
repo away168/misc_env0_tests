fc2fada72e9684def5417e77cfab51f64e14c17b
Andrew Way <andrew.way@env0.com>
2024-01-23 12:38:16 -0800

    add hostname

--- remote_backend_test/main.tf
+++ remote_backend_test/main.tf
@@ -3,6 +3,8 @@ data "terraform_remote_state" "vpc" {
 
   config = {
     organization = "c89e9161-8350-44a5-81cf-9eb26605e195"
+    hostname = "backend.api.env0.com"
+    
     workspaces = {
       name = "VPC-57019-82697596"
     }
