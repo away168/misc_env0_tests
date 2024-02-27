9a0c585d1d13f1e4098958c8601393d22e10034e
Andrew Way <andrew.way@env0.com>
2024-02-27 14:30:24 -0800

    try module registry

--- tg-az-rg/modules/wrap-rg/main.tf
+++ tg-az-rg/modules/wrap-rg/main.tf
@@ -1,5 +1,6 @@
 module "wrap-rg" {
-  source = "../../modules/azure-rg"
+  source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/resource-group/azurerm"
+  version = "1.0.1"
 
   prefix = var.prefix
   location = var.location
