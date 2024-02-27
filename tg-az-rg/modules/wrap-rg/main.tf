78d30c3fcaf3054b0ddf5a49cf10ab3a5cddf8bd
Andrew Way <andrew.way@env0.com>
2024-02-27 14:31:34 -0800

    change version tag

--- tg-az-rg/modules/wrap-rg/main.tf
+++ tg-az-rg/modules/wrap-rg/main.tf
@@ -1,6 +1,6 @@
 module "wrap-rg" {
   source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/resource-group/azurerm"
-  version = "1.0.1"
+  version = "1.0.0"
 
   prefix = var.prefix
   location = var.location
