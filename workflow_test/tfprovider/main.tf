d195ab5d29bf0ab82d350c680ffa15bd57ba3e12
Andrew Way <andrew.way@env0.com>
2024-04-18 16:22:55 -0700

    using terraform for everything

--- workflow_test/tfprovider/main.tf
+++ workflow_test/tfprovider/main.tf
@@ -15,14 +15,37 @@ locals {
     project_id = "6422fc97-70f0-4358-a24f-04ebde645a52"  // dev playgroun / andrew / workflow_test
 }
 
-data "env0_template" "example" {
-  name = "test-workflow"
+## the template that the workflows use
+resource "env0_template" "template" {
+  name                   = "workflow_null_resource_template"
+  description            = "Null Resource Template"
+  repository             = "https://github.com/away168/workflow_test/template"
+  opentofu_version       = "latest"
+  type                   = "opentofu"
+  github_installation_id = 16766458
 }
+
+## the workflow temlate itself
+resource "env0_template" "workflow" {
+  name                   = "workflow_test_template"
+  description            = "Null Resource Template"
+  repository             = "https://github.com/away168/workflow_test/workflow_2"
+  type                   = "workflow"
+  github_installation_id = 16766458
+}
+
+# ## configuration used in workflow
+# resource "env0_configuration_variable" "workspace_prefix" {
+#   name        = "WORKSPACE_PREFIX"
+#   value       = "FOO"
+#   type        = "environment"
+#   template_id = env0_template.workflow.id 
+# }
 
 resource "env0_environment" "example" {
   name                       = "tf provider test of workflow"
   project_id                 = local.project_id
-  template_id                = data.env0_template.example.id
+  template_id                = env0_template.workflow.id 
   approve_plan_automatically = true
 
   sub_environment_configuration {
