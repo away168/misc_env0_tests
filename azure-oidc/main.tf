48dde679eddae984507cdc616f665cff60aaf4df
Andrew Way <andrew.way@env0.com>
2024-05-15 14:06:57 -0700

    fix provider reference

--- azure-oidc/main.tf
+++ azure-oidc/main.tf
@@ -30,7 +30,7 @@ module "resource-group" {
 }
 
 module "resource-group-test" {
-  alias = "test"
+  provider = azurerm.test
   source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/resource-group/azurerm"
   version = "1.0.3"
 
