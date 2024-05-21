e77c943519211727d5e0ad101908eb89062d4d36
Andrew Way <andrew.way@env0.com>
2024-05-21 19:38:03 -0400

    f

--- azure-oidc/storageclass/main.tf
+++ azure-oidc/storageclass/main.tf
@@ -40,11 +40,7 @@ data "azurerm_resource_group" "one" {
 
 # Generate random text for a unique storage account name
 resource "random_id" "one" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = data.azurerm_resource_group.one.name
-  }
-  byte_length = 8
+  byte_length = 5
 }
 
 # Create storage account for boot diagnostics
@@ -93,11 +89,7 @@ resource "azurerm_resource_group" "test" {
 
 # Generate random text for a unique storage account name
 resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.test.name
-  }
-  byte_length = 8
+  byte_length = 5
 }
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "this" {
