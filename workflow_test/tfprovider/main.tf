f96c15d1dcd9cbc65a606cf09f33440f680d05e5
Andrew Way <andrew.way@env0.com>
2024-06-12 14:37:36 -0700

    remove dependencies

--- workflow_test/tfprovider/main.tf
+++ workflow_test/tfprovider/main.tf
@@ -121,7 +121,7 @@ resource "env0_environment" "example" {
     }
   }
 
-  depends_on = [ env0_template_project_assignment.assignment, env0_configuration_variable.workspace_prefix_template ]
+  depends_on = [ ]
 }
 
 variable "vpc_config_var" {
